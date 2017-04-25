13e18634199d1453d869405feec081f573bc061c
jiangping1314 <1160873948@qq.com>
2017-04-25 19:13:17 +0800

    完成待评价列表（发起评价、我的评价、我的收藏待开发）

--- app/src/main/java/cn/meituan/jp/adapter/WaitingEvaluationListAdapter.java
+++ app/src/main/java/cn/meituan/jp/adapter/WaitingEvaluationListAdapter.java
@@ -9,14 +9,13 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import cn.meituan.jp.R;
-import cn.meituan.jp.activity.MainActivity;
+import cn.meituan.jp.activity.EvaluationListActivity;
 import cn.meituan.jp.entity.OrderEntity;
 import cn.meituan.jp.utils.DateUtils;
 
@@ -24,21 +23,22 @@ import cn.meituan.jp.utils.DateUtils;
  * Created by 11608 on 2017/4/24.
  */
 
-public class OrdersListAdapter extends BaseRecyclerAdapter<OrderEntity> {
+public class WaitingEvaluationListAdapter extends BaseRecyclerAdapter<OrderEntity> {
 
 
     String[] images = new String[]{"http://img0.imgtn.bdimg.com/it/u=3083783834,572394683&fm=214&gp=0.jpg", "http://img2.imgtn.bdimg.com/it/u=4117982762,2906562027&fm=214&gp=0.jpg", "http://img.dangaoss.com/public/b/80/8/40967_m.jpg", "http://awb.img.xmtbang.com/img/uploadnew/201510/23/5f91a84b11c345a791e2c7c3f0ff283f.jpg"};
     String foodInfo = "共%s件商品,实付￥%s";
-    String []shopName = new String[]{"德克士(中和店)","大蓉和(世纪城店)","清粥小菜(天府店)","小龙坎火锅(春熙路店)"};
+    String[] shopName = new String[]{"德克士(中和店)", "大蓉和(世纪城店)", "清粥小菜(天府店)", "小龙坎火锅(春熙路店)"};
 
-    public OrdersListAdapter(Context context) {
+
+    public WaitingEvaluationListAdapter(Context context) {
         super(context);
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_order, parent, false));
+        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_waiting_evaluation, parent, false));
     }
 
     @Override
@@ -50,12 +50,12 @@ public class OrdersListAdapter extends BaseRecyclerAdapter<OrderEntity> {
             Picasso.with(context).load(images[i]).resize(320, 240).centerCrop().into(((ViewHolder) holder).ivShopImage);
             ((ViewHolder) holder).tvOrderCreateTime.setText(DateUtils.dateFormatConvert(orderEntities.getCreateAt(), "MM:dd hh:mm:ss"));
             ((ViewHolder) holder).tvFoodName.setText(orderEntities.getFoodsEntity().getName());
-           ((ViewHolder) holder).tvFoodNum.setText(orderEntities.getAmount()+"");
+            ((ViewHolder) holder).tvFoodNum.setText(orderEntities.getAmount() + "");
             ((ViewHolder) holder).tvShowFoodInfo.setText(String.format(foodInfo, orderEntities.getAmount(), orderEntities.getTotalFee()));
-            ((ViewHolder) holder).btnAddOrder.setOnClickListener(new View.OnClickListener() {
+            ((ViewHolder) holder).btnWaitEvaluation.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                   context.startActivity(new Intent(context, MainActivity.class));
+                    context.startActivity(new Intent(context, EvaluationListActivity.class));
                 }
             });
         } else {
@@ -78,8 +78,8 @@ public class OrdersListAdapter extends BaseRecyclerAdapter<OrderEntity> {
         TextView tvFoodNum;
         @Bind(R.id.tv_show_food_info)
         TextView tvShowFoodInfo;
-        @Bind(R.id.btn_add_order)
-        Button btnAddOrder;
+        @Bind(R.id.btn_wait_evaluation)
+        Button btnWaitEvaluation;
 
         public ViewHolder(View itemView) {
             super(itemView);
